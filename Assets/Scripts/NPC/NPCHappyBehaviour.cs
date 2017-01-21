c542387e6604066914ba5d7b0d2e39da9959b337
Amy DiGiovanni <amydigiov@gmail.com>
2017-01-21 14:16:41 -0800

    Implemented movement for happy NPCs

--- Assets/Scripts/NPC/NPCHappyBehaviour.cs
+++ Assets/Scripts/NPC/NPCHappyBehaviour.cs
@@ -2,11 +2,39 @@
 
 public class NPCHappyBehaviour : NPCBehaviour
 {
+    Vector3 m_Origin;
+    Vector3 m_MovementDirection;
+
     public override void UpdatePosition()
     {
+        var newPosition = m_Transform.position + m_MovementDirection * m_NPCSettings.happyMovementSpeed * Time.deltaTime;
+        if (Vector3.Distance(newPosition, m_Origin) > m_NPCSettings.happyMovementRadius)
+        {
+            RandomizeMovementDirection();
+        }
+        else
+        {
+            m_Transform.position = newPosition;
+        }
     }
 
     public NPCHappyBehaviour(Transform transform, NPCSettings npcSettings) : base(transform, npcSettings)
     {
+        m_Origin = transform.position;
+        RandomizeMovementDirection();
+    }
+
+    void RandomizeMovementDirection()
+    {
+        var movementDirection = new Vector3(1f - RNG.RandomFloat() * 2f, 1f - RNG.RandomFloat() * 2f, 0f).normalized;
+        if (movementDirection.x == 0 && movementDirection.y == 0)
+        {
+            movementDirection.x = 1f;
+        }
+        if (Vector3.Dot(movementDirection, m_MovementDirection) > 0f)
+        {
+            movementDirection *= -1f;
+        }
+        m_MovementDirection = movementDirection;
     }
 }
--- Assets/Scripts/Settings/NPCSettings.cs
+++ Assets/Scripts/Settings/NPCSettings.cs
@@ -3,13 +3,16 @@
 [CreateAssetMenu(fileName = "NPCSettings", menuName = "Settings/NPCSettings")]
 public class NPCSettings : ScriptableObject
 {
-    [Header("General")]
-    [SerializeField]
-    float m_MovementSpeed;
-
     [Header("Neutral")]
     [SerializeField]
     float m_NeutralMovementRadius;
 
-    public float movementSpeed { get { return m_MovementSpeed; } }
+    [Header("Happy")]
+    [SerializeField]
+    float m_HappyMovementSpeed;
+    [SerializeField]
+    float m_HappyMovementRadius;
+
+    public float happyMovementSpeed { get { return m_HappyMovementSpeed; } }
+    public float happyMovementRadius { get { return m_HappyMovementRadius; } }
 }
