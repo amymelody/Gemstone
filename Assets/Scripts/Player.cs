63c44452dd83d158b7658531854a28b8691963d3
Amy DiGiovanni <amydigiov@gmail.com>
2017-01-20 21:53:31 -0800

    Implemented basic player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    const string k_InputAxisHorizontal = "Horizontal";
+    const string k_InputAxisVertical = "Vertical";
+
+    [SerializeField]
+    PlayerSettings m_PlayerSettings;
+
+    void Update()
+    {
+        UpdateMovement();
+    }
+
+    void UpdateMovement()
+    {
+        transform.position
+            += new Vector3(Input.GetAxis(k_InputAxisHorizontal), Input.GetAxis(k_InputAxisVertical), 0f) 
+            * m_PlayerSettings.movementSpeed
+            * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Settings/PlayerSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerSettings", menuName = "Settings/PlayerSettings")]
+public class PlayerSettings : ScriptableObject
+{
+    [SerializeField]
+    float m_MovementSpeed;
+
+    public float movementSpeed { get { return m_MovementSpeed; } }
+}
